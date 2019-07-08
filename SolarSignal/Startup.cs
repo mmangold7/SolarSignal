d2cc76484d76a59befc7957de302824e30622345
mmangold7 <Matthew_Mangold@unifirst.com>
2019-07-08 15:27:45 -0400

    adding signalr server and client libs, adding signalr config to startup.cs, creating basic hub, creating js connection handler file, adding tutorial code as POC of messaging

--- SolarSignal/Startup.cs
+++ SolarSignal/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SolarSignal.Hubs;
 
 namespace SolarSignal
 {
@@ -33,6 +34,8 @@ namespace SolarSignal
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +54,10 @@ namespace SolarSignal
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseSignalR(routes =>
+                           {
+                               routes.MapHub<SolarHub>("/solarHub");
+                           });
 
             app.UseMvc(routes =>
             {
